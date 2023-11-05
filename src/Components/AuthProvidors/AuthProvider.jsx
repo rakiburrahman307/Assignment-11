056eece844c292ca369ac15ed55a2aa3758b6440
rakiburrahman307 <rakiburrahman307@gmail.com>
2023-11-06 02:13:57 +0600

    login and registation pages auth compleate

--- src/Components/AuthProvidors/AuthProvider.jsx
+++ src/Components/AuthProvidors/AuthProvider.jsx
@@ -1,12 +1,48 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import auth from "./FireBase/FirebaseConfig";
 
 
 export const AuthContext = createContext();
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+    const provider = new GoogleAuthProvider();
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const authInfo ={
-        
+    // log In With Google 
+    const logInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, provider)
+    }
+    // log In With Email And Password 
+    const logInWithEmailAndPassword = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    // Create User 
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    // User State Change 
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser(currentUser);
+            } else {
+                setUser(null);
+            }
+            setLoading(false);
+        })
+        return () => unSubscribe();
+    }, []);
+
+    const authInfo = {
+        user,
+        loading,
+        logInWithGoogle,
+        logInWithEmailAndPassword,
+        createUser,
     }
     return (
         <AuthContext.Provider value={authInfo}>
